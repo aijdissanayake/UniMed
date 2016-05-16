48d09edde48ced33ee7e96f906b1e3e7caa4da59
Chanuka Wijayakoon <chanuka.14@cse.mrt.ac.lk>
2016-05-16 14:36:24 +0530

    fixed assistant views into a layout

--- resources/views/assistant/index.blade.php
+++ resources/views/assistant/index.blade.php
@@ -1,22 +1,15 @@
-<!DOCTYPE HTML>
-<html>
+@extends('layouts.appLayout')
 
-<head>
+@section('header')
   <title>Unicare - Home</title>
-  <meta name="description" content="website description" />
-  <meta name="keywords" content="website keywords, website keywords" />
-  <meta http-equiv="content-type" content="text/html; charset=windows-1252" />
-  <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Tangerine&amp;v1" />
-  <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz" />
-  <link rel="stylesheet" type="text/css" href="style/style.css" />
-</head>
+@stop
 
+@section('body')
 <body>
   <div id="main">
     <div id="header">
       <div id="logo">
         <h1>Unicare Medical</h1>
-        <div class="slogan"><img src="style/logo.png" /></div>
       </div>
       <div id="menubar">
         <ul id="menu">
@@ -67,4 +60,4 @@
     </div>
   </div>
 </body>
-</html>
+@stop
