dbc14f7ae87f7b7fc19dfdb10a3b2ba89d0b2da8
Achala Dissanayake <aijdissanayake@gmail.com>
2016-05-17 21:54:30 +0530

    further appointment

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Input;
 
+use Illuminate\Support\Facades\DB;
+
 //use Illuminate\Support\Facades\Auth;
 
 class PatientController extends Controller
@@ -25,12 +27,24 @@ class PatientController extends Controller
     
     public function createAppointment(){
         
+        //takes inputs from the form
         $inputs = Input::all();
         $appDate = $inputs['appointmentDate'];
         $appSession = $inputs['session'];
+        // get the id of the current user
         $id = \Illuminate\Support\Facades\Auth::user()->id ;
-        print  $appDate ."  ". $appSession ." " . $id;
-         
+        
+        print  $appDate ."  ". $appSession ." " . $id."<br>";
+        // check whether 
+        $hasAppointment = \App\patient::where('user_id','LIKE', $id)->get()[0]->hasAppointment;
+         print $hasAppointment . "<br>";
+        
+        if ($hasAppointment == 0) {
+            DB::table('patients')
+            ->where('user_id', $id)
+            ->update(['hasAppointment' => TRUE]);
+        } 
+        echo 'new '.\App\patient::where('user_id','LIKE', $id)->get()[0]->hasAppointment;;
     }
     
 }
