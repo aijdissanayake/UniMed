7eac7df8eb4aa7689a3f465bb2075bca0273777b
Achala Dissanayake <aijdissanayake@gmail.com>
2016-09-09 12:50:47 +0530

    just

--- public/js/init.js
+++ public/js/init.js
@@ -5,12 +5,9 @@ $(document).ready(function () {
 
     $('select').material_select();
 
-    var d = new Date();
-    d.setDate(d.getDate() + 1);
     $('.datepicker').pickadate({
         selectMonths: true, // Creates a dropdown to control month
         selectYears: 15, // Creates a dropdown of 15 years to control year
-        min: d, // Enable dates after today
         close: 'Select' // rename close button
     });
 
