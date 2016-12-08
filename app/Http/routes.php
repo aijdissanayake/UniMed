166a577f56fdf5c485bc935caf068ac17df01053
Lahiru de Alwis <lahirud.14@cse.mrt.ac.lk>
2016-12-08 19:33:24 +0530

    adding routes to manage doctors

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -54,6 +54,10 @@ Route::group(['middleware' => 'authorizer:doctor'], function() {
     Route::get('doc/settings/appointments', ['as'=>'docAppSettings', 'uses'=>'DoctorController@viewAppointmentSettingsPage']);
     Route::post('doc/settings/appointments/sessionAdded',['as'=>'addSession', 'uses'=>'DoctorController@addSession']);
     Route::post('doc/settings/appointments/unavailability',['as'=>'unavailablePeriod','uses'=>'DoctorController@unavailablePeriod']);
+    Route::get('doc/settings/manageDoctors',['as' => 'manageDoctors',  function(){
+        return view('doctor.settings.manageDoctors');
+    }]);
+
 
     Route::get('doc/patients/view/{id}', ['as' => 'viewPatient', 'uses' => 'DoctorController@viewPatientDetails']);
     Route::get('doc/patients/createRecord/{id}', ['as' => 'createPatientVisitRecord', 'uses'=>'DoctorController@createPatientVisitRecord']);
