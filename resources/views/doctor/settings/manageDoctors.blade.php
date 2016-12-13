13dda908dbb0f9aa2971221071ec7a487fe6deb0
Lahiru de Alwis <lahirud.14@cse.mrt.ac.lk>
2016-12-13 23:37:04 +0530

    fml

--- resources/views/doctor/settings/manageDoctors.blade.php
+++ resources/views/doctor/settings/manageDoctors.blade.php
@@ -47,6 +47,18 @@
                 </div>
                     
                 </div>
+
+
+                <div class="fixed-action-btn">
+                <a class="btn-floating btn-large red">
+                  <i class="large material-icons">add</i>
+                </a>
+                <ul>
+                  <li><a class="btn-floating red"><i class="material-icons">recent_actors</i></a></li>
+                  <li><a class="btn-floating yellow darken-1"><i class="material-icons">perm_identity</i></a></li>
+                  
+                </ul>
+              </div>
                 
             </div>
 
